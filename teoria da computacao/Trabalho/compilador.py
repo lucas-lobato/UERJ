1decba224419e1fd8035197cd45e05c9efbe2702
Your lucas lobato <lucaslobato1999@gmail.com>
2023-01-03 14:05:42 -0300

    update

--- teoria da computacao/Trabalho/compilador.py
+++ teoria da computacao/Trabalho/compilador.py
@@ -1,31 +1,61 @@
 from string import *
+import re
 
-class program_struct:
-    def __init__(self,program, var, begin, end, iff, then, elsee, whilee, do, intt) -> None:
-        self.program = program
-        self.var = var
-        self.begin = begin
-        self.end = end
-        self.iff = iff
-        self.then = then
-        self.elsee = elsee
-        self.whilee = whilee
-        self.do = do
-        self.intt = intt
     
 def datainput():
     programa = "program correto; int a, b; begin a:=-2; b:=10 begin if (a<1) then b:=1 end end."
-    return programa
+    dado = programa.lower().split(' ')
+    return dado
 
 def analisa():
-    programa = datainput()
+    dado = datainput()
+    print(dado)
     begin = 0
     end = 0
-    letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r'
-               ,'s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J'
-               ,'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    for i in programa:
-        if i in letters:
-            print("letter",i)
+    padrao_operadores = re.compile("([a-z][0-9]?[:]?){1,32}([=]?[<>]?[<]?[<=]?[>]?[+]?[-]?[*]?[div]?){1}([a-z]?[0-9]?){1,32}")
+    tokens = {
+        'program':'<programa>',
+        'var':'<atribuição>',
+        'begin':'<comando composto>',
+        'end':'<comando composto>',
+        'if':'<comando condicional>',
+        'then':'<comando condicional>',
+        'else':'<comando condicional>',
+        'while':'<comando repetitivo>',
+        'do':'<comando repetitivo>',
+        'int':'<atribuição>'
+    }
+    operadores = {
+        '=':'Operador de relação', 
+        '<>':'Operador de relação',
+        '<':'Operador de relação',
+        '<=':'Operador de relação',
+        '>=':'Operador de relação',
+        '>':'Operador de relação',
+        '+':'Operador aritmetico',
+        '-':'Operador aritmetico',
+        '*':'Operador aritmetico',
+        'div':'Operador aritmetico'
+    }
+    for i in dado:
+        if i in tokens:
+            print(f'<{i}>, ',tokens[i])
+            if i == 'begin':
+                print('<bloco>')
+        else:
+            busca = padrao_operadores.search(i)
+            if busca:
+                if ':' in i:
+                    aux1 = i.split(':')
+                    print(f'<{aux1[0]}>','<Identificador>')
+                else:
+                    if '<' in i:
+                        aux1 = i.split('<')
+                        print(f'<{aux1[0]}>','<Identificador>')
+                    if '>' in i:
+                        aux1 = i.split('>')
+                        print(f'<{aux1[0]}>','<Identificador>')
+                #print()
+                #print(,'<Numero>')
                 
 analisa()
