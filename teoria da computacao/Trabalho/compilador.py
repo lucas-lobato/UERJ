aaf6ad019e9cc79fe0523f448929d0eb316f28d7
Your lucas lobato <lucaslobato1999@gmail.com>
2023-01-03 15:03:19 -0300

    update

--- teoria da computacao/Trabalho/compilador.py
+++ teoria da computacao/Trabalho/compilador.py
@@ -9,24 +9,23 @@ def datainput():
 
 def analisa():
     dado = datainput()
-    print(dado)
     begin = 0
     end = 0
     padrao_operadores = re.compile("([a-z][0-9]?[:]?){1,32}([=]?[<>]?[<]?[<=]?[>]?[+]?[-]?[*]?[div]?){1}([a-z]?[0-9]?){1,32}")
     tokens = {
-        'program':'<programa>',
-        'var':'<atribuição>',
-        'begin':'<comando composto>',
-        'end':'<comando composto>',
-        'if':'<comando condicional>',
-        'then':'<comando condicional>',
-        'else':'<comando condicional>',
-        'while':'<comando repetitivo>',
-        'do':'<comando repetitivo>',
-        'int':'<atribuição>'
+        'program':'programa',
+        'var':'atribuição',
+        'begin':'comando composto',
+        'end':'comando composto',
+        'if':'comando condicional',
+        'then':'comando condicional',
+        'else':'comando condicional',
+        'while':'comando repetitivo',
+        'do':'comando repetitivo',
+        'int':'atribuição'
     }
     operadores = {
-        '=':'Operador de relação', 
+        ':=':'Operador de relação', 
         '<>':'Operador de relação',
         '<':'Operador de relação',
         '<=':'Operador de relação',
@@ -39,23 +38,47 @@ def analisa():
     }
     for i in dado:
         if i in tokens:
-            print(f'<{i}>, ',tokens[i])
+            print(f'<{i}, {tokens[i]}>')
             if i == 'begin':
                 print('<bloco>')
         else:
             busca = padrao_operadores.search(i)
             if busca:
+                aux1 = ''
+                aux2 = ''
+                aux3 = ['','']
+                if '(' in i:
+                    i = i.removeprefix('(')
+                    i = i.removesuffix(')')
                 if ':' in i:
                     aux1 = i.split(':')
-                    print(f'<{aux1[0]}>','<Identificador>')
+                    aux2 = ':='
+                    aux3 = i.split('=')
+                    print(f'<{aux1[0]},','Identificador>')
                 else:
                     if '<' in i:
                         aux1 = i.split('<')
-                        print(f'<{aux1[0]}>','<Identificador>')
+                        if '=' in i:
+                            aux2 = '<='
+                            aux3 = i.split('=')
+                        else:
+                            aux2 = '<'
+                            aux3 = i.split('<')
+                        print(f'<{aux1[0]},','Identificador>')
                     if '>' in i:
                         aux1 = i.split('>')
-                        print(f'<{aux1[0]}>','<Identificador>')
-                #print()
-                #print(,'<Numero>')
+                        if '=' in i:
+                            aux2 = '>='
+                            aux3 = i.split('=')
+                        else:
+                            aux2 = '>'
+                            aux3 = i.split('>')
+                        print(f'<{aux1[0]},','Identificador>')
+                if aux2 in operadores:
+                    print(f'<{aux2}, {operadores[aux2]}>')
+                if aux3[1] != '':
+                    print(f'<{aux3[1]}, Numero>')
+            else:
+                print(i)
                 
 analisa()
