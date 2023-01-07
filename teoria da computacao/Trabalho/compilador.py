4a94084ed855cf658dea309e5c4e4d3b48dbd419
Your lucas lobato <lucaslobato1999@gmail.com>
2023-01-07 11:26:29 -0300

    update

--- teoria da computacao/Trabalho/compilador.py
+++ teoria da computacao/Trabalho/compilador.py
@@ -1,16 +1,14 @@
 # ESTE CÓDIGO IMPLEMENTA UM ANALIZADOR LÉXICO PARA O EXEMPLO DE FRAGMENTO DE LINGUAGEM APRESENTADO EM SALA DE AULA (VEJA OS SLIDES DA AULA 4: ANÁLISE LÉXICA: PARTE 2)
 # E PODERÁ SER UTILIZADO COMO PONTO DE PARTIDA PARA IMPLEMENTAÇÃO DO ANALISADOR LÉXICO PARA LINGUAGEM ADOTADA NO TRABALHO PROPOSTO.
 
-#NOME TOKENS
-from io import * 
 
+#NOME TOKENS
 IF = 256
 THEN = 257
 ELSE = 258
 RELOP = 259
 ID = 260
 NUM = 261
-
 
 #ATRIBUTOS
 LT = 262
@@ -21,16 +19,18 @@ GT = 266
 GE = 267
 
 
-
 class token:
     def __init__(self, nome_token, atributo):
         self.nome_token = nome_token
         self.atributo = atributo
+        
+        
 
 
 estado = 0
 partida = 0
 cont_sim_lido = 0
+code = ''
 
 
 def readFile(fileName):
@@ -74,8 +74,9 @@ def falhar():
 
 def proximo_token():
 
-	while code[cont_sim_lido] != EOF:
-         if estado == 0:
+    Token = token(None,None)
+    while code[cont_sim_lido] != EOFError:
+        if estado == 0:
             c = code[cont_sim_lido]
             if (c == ' ') or (c == '\n'):
                 estado = 0
@@ -90,7 +91,7 @@ def proximo_token():
                 estado = falhar()
             break
 
-         if estado == 1:
+        if estado == 1:
             cont_sim_lido += 1
             c = code[cont_sim_lido]
 
@@ -102,39 +103,39 @@ def proximo_token():
                 estado = 4
             break
 
-         if estado == 2:
+        if estado == 2:
             cont_sim_lido += 1
             print("<relop, LE>\n")
-            token.nome_token = RELOP
-            token.atributo = LE
+            Token.nome_token = RELOP
+            Token.atributo = LE
             estado = 0
-            return(token)
+            return Token
             break
 
-         if estado == 3:
+        if estado == 3:
             cont_sim_lido += 1
             print("<relop, NE>\n")
-            token.nome_token = RELOP
-            token.atributo = NE
+            Token.nome_token = RELOP
+            Token.atributo = NE
             estado = 0
-            return(token)
+            return Token
             break
 
-         if estado == 4:
+        if estado == 4:
             cont_sim_lido+= 1
             print("<relop, LT>\n")
-            token.nome_token = RELOP
-            token.atributo = LT
+            Token.nome_token = RELOP
+            Token.atributo = LT
             estado = 0
-            return(token)
+            return Token
             break
 
-         if estado == 5:
+        if estado == 5:
             cont_sim_lido+= 1
             print("<relop, EQ>\n")
-            token.nome_token = RELOP
-            token.atributo = EQ
-            return(token)
+            Token.nome_token = RELOP
+            Token.atributo = EQ
+            return Token
             break
 
         if estado == 6:
@@ -146,87 +147,77 @@ def proximo_token():
                 estado = 8
             break
 
-  if estado == 7:
-      cont_sim_lido+= 1
-      print("<relop, GE>\n")
-      token.nome_token = RELOP
-      token.atributo = GE
-      estado = 0
-      return(token)
-      break
+    if estado == 7:
+        cont_sim_lido+= 1
+        print("<relop, GE>\n")
+        Token.nome_token = RELOP
+        Token.atributo = GE
+        estado = 0
+        return Token
 
-  if estado == 8:
-      cont_sim_lido+= 1
-      print("<relop, GT>\n")
-      token.nome_token = RELOP
-      token.atributo = GT
-      #ATENÇÃO - CORREÇÃO: foi acrescentado o comando "estado = 0"
-      estado = 0
-      return(token)
-      break
+    if estado == 8:
+        cont_sim_lido+= 1
+        print("<relop, GT>\n")
+        Token.nome_token = RELOP
+        Token.atributo = GT
+        #ATENÇÃO - CORREÇÃO: foi acrescentado o comando "estado = 0"
+        estado = 0
+        return Token
 
-  if estado == 9:
-      c = code[cont_sim_lido]
-      if((c == ' ') or (c == '\n')):
-          estado = 0
-          cont_sim_lido += 1
+    if estado == 9:
+        c = code[cont_sim_lido]
+        if((c == ' ') or (c == '\n')):
+            estado = 0
+            cont_sim_lido += 1
       
-      else:
-          #implementar ações referentes aos estado */
-          estado = falhar()
-      break
+        else:
+            #implementar ações referentes aos estado */
+            estado = falhar()
 
       #implementar ações para os estados 10, 11, 12*/
 
-  if estado == 12:
-      c = code[cont_sim_lido]
-      if((c == ' ') or (c == '\n')):
-          estado = 0
-          cont_sim_lido+= 1
-      else:
-          #implementar ações referentes aos estado */
-          estado = falhar()
-      break
+    if estado == 12:
+        c = code[cont_sim_lido]
+        if((c == ' ') or (c == '\n')):
+            estado = 0
+            cont_sim_lido+= 1
+        else:
+            #implementar ações referentes aos estado */
+            estado = falhar()
 
-      #implementar ações para os estados 13-19*/
+        #implementar ações para os estados 13-19*/
 
-  if estado == 20:
-      c = code[cont_sim_lido]
-      if((c == ' ') or (c == '\n')):
-          estado = 0
-          cont_sim_lido+= 1
-      else:
-          #implementar ações referentes aos estado */
-          estado = falhar()
-      break
+    if estado == 20:
+        c = code[cont_sim_lido]
+        if((c == ' ') or (c == '\n')):
+            estado = 0
+            cont_sim_lido+= 1
+        else:
+            #implementar ações referentes aos estado */
+            estado = falhar()
 
-      #implementar ações para os estados 21-24*/
+        #implementar ações para os estados 21-24*/
 
-  if estado == 25:
-      c = code[cont_sim_lido]
-      if((c == ' ') or (c == '\n')):
-          estado = 0
-          cont_sim_lido+= 1
-      else:
-      
-          #implementar ações referentes aos estado */
-          estado = falhar()
-          token.nome_token = -1
-          token.atributo = -1
-          return(token)'
-            
-
-self.nome_token = EOF
-    self.atributo = -1
-    return token
-
-
-
+    if estado == 25:
+        c = code[cont_sim_lido]
+        if((c == ' ') or (c == '\n')):
+            estado = 0
+            cont_sim_lido+= 1
+        else:
+        
+            #implementar ações referentes aos estado */
+            estado = falhar()
+            Token.nome_token = -1
+            Token.atributo = -1
+            return Token
+    
+    Token.nome_token = EOFError
+    Token.atributo = -1
+    return Token
 
 
 def main():
   code = readFile("programa.txt")
-  token = proximo_token()
   token = proximo_token()
   #...
 
